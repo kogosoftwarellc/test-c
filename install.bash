71fb72db331ed805cebf389055d37b6f980a63a1
jsdevel <js.developer.undefined@gmail.com>
2017-05-31 01:14:56 -0700

    Adding sha version to install.bash

--- install.bash
+++ install.bash
@@ -17,11 +17,29 @@ function error() {
   echo "$1" >&2
 }
 
+shafile=current-sha
+oldsha=''
+
+if [[ -f "$shafile" ]]; then
+  oldsha="`cat $shafile`"
+fi
+
+if [[ -n "$oldsha" ]]; then
+  oldsha="${oldsha:0:7}"
+fi
+
 download test-c
 chmod +x test-c
 download test-c.h
 download README.md
 download LICENSE
+curl -sk \
+     -H 'Accept: application/vnd.github.VERSION.sha' \
+     https://api.github.com/repos/kogosoftwarellc/test-c/commits/HEAD \
+     | tee "$shafile" > /dev/null
+
+newsha="`cat $shafile`"
+newsha="${newsha:0:7}"
 
 sessionfile=".bashrc"
 
@@ -51,5 +69,9 @@ export PATH="$testcdir:\$PATH"
 ~
   fi
 
-  echo "test-c is ready to use!"
+  echo "test-c ($newsha) is ready to use!"
+
+  if [[ -n "$oldsha" && "$oldsha" != "$newsha" ]]; then
+    echo "... old sha was $oldsha"
+  fi
 fi
